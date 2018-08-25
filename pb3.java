6354f39cfb012ad8f3b2099a1192468202b7dbec
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-25 14:42:35 +0530

    Create pb3.java

---
+++ pb3.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int k=s.nextInt();
+//	int i=1;
+	int c=1;
+	for(int i=1;i<=k;i++)
+	{
+		
+	 c=n*c;	
+	
+	}
+	System.out.print(c);
+	
+	}
+}
