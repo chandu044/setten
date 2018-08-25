f50ed18133e1012af1418327f20c9a2f834b22d9
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-25 15:15:21 +0530

    Create pb6.java

---
+++ pb6.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int k=s.nextInt();
+	int[] a=new int[10];
+	int[] b=new int[10];
+     for(int i=1;i<=n;i++)
+     {
+     	a[i]=s.nextInt();
+     }
+     int j=1;
+     for(int i=1;i<=n;i++)
+     {
+        if(a[i]%2!=0)
+        {
+        	b[j]=a[i];
+        	j++;
+        }
+        }
+        System.out.print(b[k]);
+    }
+}
